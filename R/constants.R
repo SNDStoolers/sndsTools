080da13fa0c9dd9749ab57843fca044dd01c2ad7
Matthieu Doutreligne <matt.dout@gmail.com>
2026-04-27 17:35:33 +0200

    constants: mv from constants() function to variable exports

--- R/constants.R
+++ R/constants.R
@@ -1,18 +1,19 @@
-#' @title Stocke des constantes pour sndsTools
+# Fichier pour stocker les constantes du paquet sndsTools
 
-#' @return A list of constants for sndsTools.
+# Constantes exportées ie. accessibles par les utilisateurs du paquet
+
+#' @title Est-ce que le code tourne sur le portail de la CNAM ?
 #' @export
 #' @family utils
-constants_snds_tools <- function() {
-  constants <- list()
-  constants$is_portail <- dir.exists("~/sasdata1")
-  constants
-}
+IS_PORTAIL <- dir.exists("~/sasdata1")
+
+# Constantes non exportées ie. utilisées uniquement en interne dans le paquet
 
 DIR2SNDS_CACHE <- "~/.cache/sndsTools"
 if (!dir.exists(DIR2SNDS_CACHE)) {
   dir.create(DIR2SNDS_CACHE, recursive = TRUE)
 }
+
 PATH2SYNTHETIC_SNDS <- file.path(
   DIR2SNDS_CACHE,
   "synthetic_snds.duckdb"
